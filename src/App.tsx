19bc1457731372e9883a7e3550091f41619c6aaa
SASAKI Shunsuke <erukiti@gmail.com>
2019-09-16 10:49:49 +0900

    MonacoEditorを動かすようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+
+import "monaco-editor/esm/vs/language/typescript/monaco.contribution";
+import "monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution";
+import "monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution";
 
 const App: React.FC = () => {
+  const editorDiv = React.useRef<HTMLDivElement>(null);
+  const editorRef = React.useRef<monaco.editor.IStandaloneCodeEditor>();
+
+  React.useLayoutEffect(() => {
+    editorRef.current = monaco.editor.create(editorDiv.current!, {
+      automaticLayout: true,
+      value: "const hoge = 1\n",
+      language: "javascript"
+    });
+    editorRef.current.focus();
+    return () => {
+      if (editorRef.current) {
+        editorRef.current.dispose();
+        editorRef.current = undefined;
+      }
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <div ref={editorDiv} style={{ height: "100%" }} />
     </div>
   );
-}
+};
 
 export default App;
