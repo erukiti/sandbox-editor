03824075055d487180e7614d2b05791359029b20
SASAKI Shunsuke <erukiti@gmail.com>
2019-09-17 13:37:03 +0900

    Sandboxを動作するように

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import { runJSTest } from "./sandbox/javascript";
+
+const initialSources: { [p: string]: string } = {
+  "index.test.js": `const { answer } = require('index.js')
+
+describe('computed on The Earth', () => {
+  test('Life, the Universe, and Everything is 42.', () => {
+    expect(answer()).toBe(42)
+  })
+})
+`,
+  "index.js": `function answer() {
+  return 8 * 6;
+}
+
+module.exports = {
+  answer
+}
+`
+};
 
 const App: React.FC = () => {
+  const [stdout, setStdout] = useState("");
+
+  useEffect(() => {
+    runJSTest(initialSources, "index.test.js", setStdout);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <code>
+      <pre>{stdout}</pre>
+    </code>
   );
-}
+};
 
 export default App;
